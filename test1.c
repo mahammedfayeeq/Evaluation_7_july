ab157ad3c2a487e59dd61e8966d164c8b5e0d76d
mahammedfayeeq <mahammed.tumminakatti@s.amity.edu>
2025-07-07 15:36:56 +0530

    about parents

--- test1.c
+++ test1.c
@@ -4,3 +4,4 @@ int main()
         printf("hello my name is mahammed fayeeq");
 	printf("i am from haveri");
 	printf("my hobbies are playing cricket,volleyball");
+	printf("my father name is mahammed yunis and mother name is tabassum");
