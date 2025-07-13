3e0c99e3e6d0556af65363ed0120f8b0bf380598
mahammedfayeeq <mahammed.tumminakatti@s.amity.edu>
2025-07-13 21:41:56 +0530

    added location

--- test1.c
+++ test1.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
  
 int main()
+{	
         printf("hello my name is mahammed fayeeq");
 	printf("i am from haveri");
 	printf("my hobbies are playing cricket,volleyball");
 	printf("my father name is mahammed yunis and mother name is tabassum");
 	printf("i am persueing my btech in amity university");
-
+        printf("it located near dc office dodballapura and devnahalli road")
 rturn 0;
+}	
